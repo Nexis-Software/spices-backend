9a301a452d9b20c0c6a5ed8e917128fd212e5796
Amantha Jayathilake <amanthajayathilake@gmail.com>
2024-08-04 10:13:08 +0530

    helmet and cors

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,27 @@ import connectDB from "./config/db";
 import productRoutes from "./routes/productRoutes";
 import errorHandler from "./middlewares/errorHandler";
 import setupSwagger from "./config/swagger";
+import cors from "cors";
+import helmet from "helmet";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+// Enable Helmet for security
+app.use(helmet());
+
+// Enable CORS
+const corsOptions = {
+  origin: "*", // Replace with your allowed origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Enable if you need to pass cookies with requests
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
